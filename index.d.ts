3cdf94fd23ac3ff5bd4c6ac5f2dab80802702c32
Stefano Buik <stefano.buik@atabix.nl>
2022-04-14 10:13:47 +0200

    typing

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,1 @@
-// we do not need actually types here, just this will do fine.
-export default class ImageResize{
-	public initializeModules(): void;
-	public onUpdate(): void;
-	public removeModules(): void;
-	public handleClick(): void;
-	public show(): void;
-	public showOverlay(): void;
-	public hideOverlay(): void;
-	public repositionElements(): void;
-	public hide(): void;
-	public setUserSelect(): void;
-	public checkImage(): void;
-};
+export default class ImageResize {}
